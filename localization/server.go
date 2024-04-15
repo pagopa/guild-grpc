e89f4394f0684a2c79853e87d2c62f19bb032dcc
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-04-15 18:15:17 +0200

    feat: add localization impl

--- localization/server.go
+++ localization/server.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
+	pb "localization/protofiles/localization"
 	"log"
 	"net"
-
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 
 	"google.golang.org/grpc"
 )
@@ -18,25 +16,39 @@ var (
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
-	pb.UnimplementedGreeterServer
+	pb.UnimplementedLocalizationServer
 }
 
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.GetName())
-	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+func (s *server) SendLocation(stream pb.Localization_SendLocationServer) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+
+		// Handle vehicle location request
+		log.Printf("Received vehicle location request: VehicleID %s, Latitude %f, Longitude %f\n",
+			req.VehicleId, req.Location.Latitude, req.Location.Longitude)
+
+		// Send acknowledgement
+		if err := stream.SendAndClose(&pb.AckResponse{Success: true, Message: "Vehicle location received"}); err != nil {
+			return err
+		}
+	}
 }
 
 func main() {
-    fmt.Println("Hello, World!")
-	
+	fmt.Println("gRPC guild localization µ-service has been launched!")
+
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
+	pb.RegisterLocalizationServer(s, &server{})
+
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
