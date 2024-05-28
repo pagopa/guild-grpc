8a6b3ace9181593503c4e8619a4e753642f15c7f
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2024-05-28 11:47:02 +0200

    FEAT: openapi

--- booking/src/main/java/it/pagopa/guild/grpc/booking/exception/BookingRestExceptionHandler.java
+++ booking/src/main/java/it/pagopa/guild/grpc/booking/exception/BookingRestExceptionHandler.java
@@ -1,9 +1,10 @@
 package it.pagopa.guild.grpc.booking.exception;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import it.pagopa.guild.grpc.booking.controller.BookingController;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -23,14 +23,20 @@ import it.pagopa.guild.grpc.booking.utils.Constants;
 public class BookingRestExceptionHandler {
 
     @Data
-    @Builder
+    @SuperBuilder
     public static class ErrorResponse {
-        private String message;
+        @Schema(description = "Feedback about the success of the operation", example = "false", requiredMode = Schema.RequiredMode.REQUIRED)
+        Boolean success;
+
+        @Schema(description = "Error message", example = "Vehicle booking failed")
+        String message;
     }
 
+    @EqualsAndHashCode(callSuper = true)
     @Data
-    @Builder
-    public static class ValidationErrorResponse {
+    @SuperBuilder
+    public static class ValidationErrorResponse extends ErrorResponse {
+        @Schema(description = "Map of error messages", example = "{\"userId\": \"UserId is required\"}")
         private Map<String, String> errors;
     }
 
@@ -45,14 +51,14 @@ public class BookingRestExceptionHandler {
     ResponseEntity<ErrorResponse> handleResourceNotAvailableExceptionError(ResourceNotAvailableException e) {
         log.error("Vehicle booking failed", e);
         return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(ErrorResponse.builder().message(Constants.VEHICLE_ALREADY_BOOKED).build());
+                .body(ErrorResponse.builder().success(false).message(Constants.VEHICLE_ALREADY_BOOKED).build());
     }
 
     @ExceptionHandler(BookingConfirmationException.class)
     ResponseEntity<ErrorResponse> handleBookingConfirmationExceptionError(BookingConfirmationException e) {
         log.error("vehicle sendBookConfirmation REST failed", e);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ErrorResponse.builder().message(e.getError()).build());
+        return ResponseEntity.status(HttpStatus.BAD_GATEWAY)
+                .body(ErrorResponse.builder().success(false).message(e.getError()).build());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -62,13 +68,16 @@ public class BookingRestExceptionHandler {
                         error -> ((FieldError) error).getField(),
                         error -> error.getDefaultMessage()));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ValidationErrorResponse.builder().errors(errors).build());
+                .body(ValidationErrorResponse.builder()
+                        .success(false)
+                        .message(Constants.VEHICLE_ALREADY_BOOKED)
+                        .errors(errors).build());
     }
 
     @ExceptionHandler(Exception.class)
     ResponseEntity<ErrorResponse> handleGenericError(Exception e) {
         log.error("Generic error", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ErrorResponse.builder().message(Constants.GENERIC_ERROR).build());
+                .body(ErrorResponse.builder().success(false).message(Constants.GENERIC_ERROR).build());
     }
 }
