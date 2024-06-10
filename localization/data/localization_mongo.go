0615200d7944f0f917d9519087aa3116e4e8db2c
Angelo Caporaso <56113767+AngeloCaporaso@users.noreply.github.com>
2024-06-10 18:29:54 +0200

    refactoring: Update GET near vehicles (#23)

--- localization/data/localization_mongo.go
+++ localization/data/localization_mongo.go
@@ -1,4 +1,4 @@
-package localization_mongo
+package data
 
 import (
 	"context"
--- localization/main.go
+++ localization/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	grpc "localization/localization_grpc"
-	rest "localization/localization_rest"
+	"localization/server"
 )
 
 func main() {
 	fmt.Println("gRPC guild localization µ-service has been launched!")
 	// Start GIN server in a Goroutine
-	go rest.StartGINServer()
+	go server.StartGINServer()
 	// Start gRPC server in the main Goroutine
-	grpc.StartGRPCServer()
+	server.StartGRPCServer()
 }
